29da2f7fa0f6a2ad94b7716d1ace60dee5626dcc
Matthew <m.bednarski@gmail.com>
2022-01-11 00:44:20 +0000

    Function to pop out the first item in a given array, and an assertion function to ensure that it is working properly

---
+++ head.js
@@ -0,0 +1,19 @@
+// assert function implemented
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// head function (to isolate the first item in an array)
+const head = function(arrayInput) {
+  return arrayInput[0];
+};
+
+// Test Code
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([1]), 1);
