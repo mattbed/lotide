e01d7dd6002b7e087fcd98b23cd9b17aa0a23c31
Matthew <m.bednarski@gmail.com>
2022-01-11 00:20:40 +0000

    an alternative to the built-in Assert function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// assert function implemented
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🎉 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("⛔ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("42", 42);
+assertEqual("This is a string","This is a string");
+assertEqual(1,100);
