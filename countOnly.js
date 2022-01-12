3206f49f0e371daa4ff5a6e60ee4197b3fc4b83e
Matthew <m.bednarski@gmail.com>
2022-01-12 21:08:22 +0000

    counts instances of elements from an object in an array

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+// assert function implemented
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// countOnly will be given an array and an object. It will return an object containing counts of everything that the input object listed.
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+// test case
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames,{ "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
