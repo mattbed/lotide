22ee5341fc75fd5563bb5753652ea683b0e5e467
mattbed <m.bednarski@gmail.com>
2022-06-17 23:13:14 -0400

    Refactor importing

--- eqObjects.js
+++ eqObjects.js
@@ -1,31 +1,5 @@
-// assert function implemented
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// compare two arrays
-
-const eqArrays = function(array1, array2) {
-  // first check if the arrays are the same length
-    if (array1.length === array2.length) {
-  // for statement to compare each individual element, flagging any mismatch    
-      for (i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-          return false;
-        }
-      }
-  // if the code passes up to this point, length will be equal and no elements will have mismatched    
-      return true;
-  // if the arrays were different length, it would skip to the following return statement
-    } else {
-      return false;
-    }
-  }
-
+const assertEqual = require('./assertEqual.js');
+const eqArrays = require('./eqArrays.js');
 
 // Returns true if both objects have identical keys with identical values
 // Otherwise you get false
