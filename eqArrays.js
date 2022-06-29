1b78769e37d65dbad0f9ca50ff978fc68c1fd9b8
mattbed <m.bednarski@gmail.com>
2022-06-29 19:13:10 -0400

    Update eqArrays to recurse through nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,28 @@
 // compares two arrays
 const eqArrays = function(array1, array2) {
 // first check if the arrays are the same length
-  if (array1.length === array2.length) {
-    // for statement to compare each individual element, flagging any mismatch
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    // if the code passes up to this point, length will be equal and no elements will have mismatched
-    return true;
-    // if the arrays were different length, it would skip to the following return statement
+  if (array1.length !== array2.length) return false;
+      // loop to compare each individual element, flagging any mismatch
+  for (let i = 0; i < array1.length; i++) {
+    // check if both are arrays, recurse if yes
+    if (Array.isArray(array1[i]) && (Array.isArray(array2[i]))) {
+      if (!eqArrays(array1[i], array2[i])) return false;
+      // check if only one is array, signalling a mismatch thus flag as false
+    } else if (Array.isArray(array1[i]) || (Array.isArray(array2[i]))) {
+      return false;
+      // lastly compare the two non-array values, flagging as false if they do not match
+    } else if (array1[i] !== array2[i]) {
+      return false;
+    };
   }
-  return false;
+  // if nothing was flagged as false, return true
+  return true;
 };
+
+console.log("true = ", eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+console.log("false = ", eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+console.log("false = ", eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
+console.log("false = ", eqArrays([[2, 3], 5], [[2, 3], 4])) // => false
+console.log("true = ", eqArrays([[2, 3, [5]], [4]], [[2, 3, [5]], [4]])) // => true
 
 module.exports = eqArrays;
